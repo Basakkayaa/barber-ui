9728b21ee52ab748d72d378ac0500e1befbb50a9
Basakkayaaa <basakkayaaa6@gmail.com>
2024-02-02 20:14:52 +0300

    first commit

---
+++ src/app/modules/admin/order/order-add/order-add.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-add',
+  standalone: true,
+  imports: [],
+  templateUrl: './order-add.component.html',
+  styleUrl: './order-add.component.scss'
+})
+export class OrderAddComponent {
+  createOrderForm: any;
+
+}
